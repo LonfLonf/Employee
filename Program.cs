3df7b3919e8691c5d06f57a3ee4e0a0e4316422b
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2025-01-07 07:51:33 -0300

    CRUD

--- Program.cs
+++ Program.cs
@@ -5,74 +5,121 @@ namespace CrudExample
 {
     class Program
     {
-        string connectionString = "Server=LONFTONF;Database=CrudExample;User Id=LONFTONF;Password=;";
 
         public static void Main(string[] args)
         {
+            
+            string connectionString = @"Data Source=LONFTONF;
+                            Initial Catalog=CrudExample;
+                            Trusted_Connection=Yes;";
 
-        }
-
-        void CreateEmployee(string name, string position, decimal salary)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            while (true)
             {
-                string query = "INSERT INTO Employees (Name, Position, Salary) VALUES (@Name, @Position, @Salary)";
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@Name", name);
-                command.Parameters.AddWithValue("@Position", position);
-                command.Parameters.AddWithValue("@Salary", salary);
+                Console.WriteLine("Choose An Operation:");
+                Console.WriteLine("1. Create Employee");
+                Console.WriteLine("2. Read Employees");
+                Console.WriteLine("3. Update Employee");
+                Console.WriteLine("4. Delete Employee");
+                Console.WriteLine("5. Exit");
+                int choice = int.Parse(Console.ReadLine());
 
-                connection.Open();
-                command.ExecuteNonQuery();
-                Console.WriteLine("Employee Created Successfully!");
-            }
-        }
-
-        void ReadEmployees()
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string query = "SELECT * FROM Employees";
-                SqlCommand command = new SqlCommand(query, connection);
-
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
+                switch (choice)
                 {
-                    Console.WriteLine($"Id: {reader["Id"]}, Name: {reader["Name"]}, Position: {reader["Position"]}, Salary: {reader["Salary"]}");
+                    case 1:
+                        Console.Write("Name: ");
+                        string name = Console.ReadLine();
+                        Console.Write("Position: ");
+                        string position = Console.ReadLine();
+                        Console.Write("Salary: ");
+                        decimal salary = decimal.Parse(Console.ReadLine());
+                        CreateEmployee(name, position, salary);
+                        break;
+                    case 2:
+                        ReadEmployees();
+                        break;
+                    case 3:
+                        Console.Write("Employee ID to update: ");
+                        int updateId = int.Parse(Console.ReadLine());
+                        Console.Write("Name: ");
+                        string updateName = Console.ReadLine();
+                        Console.Write("Position: ");
+                        string updatePosition = Console.ReadLine();
+                        Console.Write("Salary: ");
+                        decimal updateSalary = decimal.Parse(Console.ReadLine());
+                        UpdateEmployee(updateId, updateName, updatePosition, updateSalary);
+                        break;
+                    case 4:
+                        Console.Write("Employee ID to delete: ");
+                        int deleteId = int.Parse(Console.ReadLine());
+                        DeleteEmployee(deleteId);
+                        break;
+                    case 5:
+                        return;
                 }
-            }
-        }
 
-        void UpdateEmployee(int id, string name, string position, decimal salary)
-        {
-            using(SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string query = "UPDATE Employees SET Name = @Name, Position = @Position, Salary = @Salary WHERE Id = @Id";
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@Id", id);
-                command.Parameters.AddWithValue("@Name", name);
-                command.Parameters.AddWithValue("@Position", position);
-                command.Parameters.AddWithValue("@Salary", salary);
+                void CreateEmployee(string name, string position, decimal salary)
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        string query = "INSERT INTO Employees (Name, Position, Salary) VALUES (@Name, @Position, @Salary)";
+                        SqlCommand command = new SqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@Name", name);
+                        command.Parameters.AddWithValue("@Position", position);
+                        command.Parameters.AddWithValue("@Salary", salary);
 
-                connection.Open();
-                command.ExecuteNonQuery();
-                Console.WriteLine("Employee updated successfully!");
-            }
-        }
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Employee Created Successfully!");
+                    }
+                }
 
-        void DeleteEmployee(int id)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string query = "DELETE FROM Employees WHERE Id = @Id";
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@Id", id);
+                void ReadEmployees()
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        string query = "SELECT * FROM Employees";
+                        SqlCommand command = new SqlCommand(query, connection);
 
-                connection.Open();
-                command.ExecuteNonQuery();
-                Console.WriteLine("Employee deleted Successfully!");
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"Id: {reader["Id"]}, Name: {reader["Name"]}, Position: {reader["Position"]}, Salary: {reader["Salary"]}");
+                        }
+                    }
+                }
+
+                void UpdateEmployee(int id, string name, string position, decimal salary)
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        string query = "UPDATE Employees SET Name = @Name, Position = @Position, Salary = @Salary WHERE Id = @Id";
+                        SqlCommand command = new SqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@Id", id);
+                        command.Parameters.AddWithValue("@Name", name);
+                        command.Parameters.AddWithValue("@Position", position);
+                        command.Parameters.AddWithValue("@Salary", salary);
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Employee updated successfully!");
+                    }
+                }
+
+                void DeleteEmployee(int id)
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        string query = "DELETE FROM Employees WHERE Id = @Id";
+                        SqlCommand command = new SqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@Id", id);
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Employee deleted Successfully!");
+                    }
+                }
             }
         }
     }
--- obj/Debug/net9.0/test.AssemblyInfo.cs
+++ obj/Debug/net9.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b02ee521874f1d73175771f722cad1c3d2f19daf")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
